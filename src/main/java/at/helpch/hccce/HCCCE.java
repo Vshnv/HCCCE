02692da64e3fa74af6443c6a605ae94cd53723e4
extendedclip <fairviewent@yahoo.com>
2020-06-30 10:41:08 -0400

    Initial commit no .gitignore

---
+++ src/main/java/at/helpch/hccce/HCCCE.java
@@ -0,0 +1,17 @@
+package at.helpch.hccce;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HCCCE extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
